ec9a3c9490745a252bbe81777a06e2cb540c81ff
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-04-17 12:48:02 +0300

    Initial Commit

---
+++ Pairs/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Pairs
+//
+//  Created by ramil on 17.04.2020.
+//  Copyright © 2020 com.ri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
